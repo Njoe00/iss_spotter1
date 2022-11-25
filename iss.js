3bb66ee9c5f1df6efdbc2a1518cc3c0ce1a51e56
Njoe00 <nick.james.joe@gmail.com>
2022-11-25 01:05:44 +0000

    created function that checks the IP address of your geo coordinates and returns the latitude & longitude of your location

--- iss.js
+++ iss.js
@@ -1,24 +1,43 @@
 const request = require("request");
 
+// const fetchMyIP = function(callback) {
+//   request("https://api.ipify.org?format=json", (err, response, body) => {
+//     if (err) {
+//       callback(err, null);
+//       return;
+//     }
+//     // if non-200 status, assume server error
+//     if (response.statusCode !== 200) {
+//       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+//       callback(Error(msg), null);
+//       return;
+//     } else {
+//       const ip = JSON.parse(body).ip;
+//       callback(null, ip);
+//     }
+//   });
+// };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`,(err, response, body) => {
 
-const fetchMyIP = function(callback) {
-  request("https://api.ipify.org?format=json", (err, response, body) => {
     if (err) {
       callback(err, null);
       return;
     }
-    // if non-200 status, assume server error
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+    const jsonBody = JSON.parse(body)
+
+    if (!jsonBody.success) {
+      const msg = `Failure message: ${jsonBody} for IP ${jsonBody.ip}`;
       callback(Error(msg), null);
       return;
-    } else {
-      const ip = JSON.parse(body).ip;
-      callback(null, ip);
     }
+
+    const {latitude, longitude} = jsonBody;
+
+    callback(null, {latitude, longitude});
   });
 };
 
 
-module.exports = {fetchMyIP};
+module.exports = {fetchCoordsByIP};
